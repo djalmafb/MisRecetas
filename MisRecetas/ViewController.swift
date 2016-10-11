e0b4b44ef1cb5f89893309a43cb49d4559f2e292
Djalma Bueno <djalmabueno@macbook-pro-de-djalma.local>
2016-10-11 14:04:05 -0300

    commit

--- MisRecetas/ViewController.swift
+++ MisRecetas/ViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Juan Gabriel Gomila Salas on 31/8/16.
 //  Copyright © 2016 Juan Gabriel Gomila Salas. All rights reserved...
+// djama testando as mudanças do GitHub
 //
 
 import UIKit
